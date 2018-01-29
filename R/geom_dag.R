88079325133bc8a41fa6592e788ede6cd0f0536f
Malcolm Barrett <malcolmbarrett@gmail.com>
2018-01-28 23:11:22 -0800

    rebuild previous examples

--- R/geom_dag.R
+++ R/geom_dag.R
@@ -393,6 +393,18 @@ geom_dag_edges <- function(mapping = NULL, data_directed = NULL, data_bidirected
 #'
 #' @export
 #'
+#' @examples
+#' library(dagitty)
+#' dagify(m ~ a + b, x ~ a, y ~ b) %>%
+#'   tidy_dagitty() %>%
+#'   control_for("m") %>%
+#'   ggplot(aes(x = x, y = y, xend = xend, yend = yend, shape = adjusted)) +
+#'     geom_dag_edges() +
+#'     geom_dag_collider_edges() +
+#'     geom_dag_node() +
+#'     geom_dag_text() +
+#'     theme_dag() +
+#'     scale_dag()
 geom_dag_collider_edges <- function(mapping = NULL, data = NULL,
                                     stat = "identity", position = "identity",
                                     ...,
