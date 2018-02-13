64ce35cbda24fdca61ad005c15c1ade9e74361fc
Malcolm Barrett <malcolmbarrett@gmail.com>
2018-02-13 14:55:52 -0800

    fix typo in method names

--- R/dplyr_methods.R
+++ R/dplyr_methods.R
@@ -1,161 +1,194 @@
+## dplyr methods:
+
+#' Dplyr verb methods for \code{tidy_dagitty} objects
+#'
+#' Dplyr verb methods for \code{tidy_dagitty} objects.
+#' @param .data data object of class \code{tidy_dagitty}
+#' @param .dots,x,y,by,copy,suffix,.keep_all see corresponding function in
+#'   package \code{dplyr}
+#' @param ... other arguments passed to the \code{dplyr} function
+#'
+#' @name dplyr
+#' @examples
+#' library(dplyr)
+#' tidy_dagitty(m_bias()) %>%
+#'   group_by(name) %>%
+#'   summarize(n = n())
 #' @export
+#' @name dplyr
 #' @importFrom dplyr select
-select.tidy_daggity <- function(.data, ...) {
+select.tidy_dagitty <- function(.data, ...) {
   .data$data <- dplyr::select(.data$data, ...)
   .data
 }
 
-# Required to export filter, otherwise:
-# Warning: declared S3 method 'filter.tbl_time' not found
-# because of stats::filter
-
 #' @export
-#'
 dplyr::filter
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr filter
-filter.tidy_daggity <- function(.data, ...) {
+filter.tidy_dagitty <- function(.data, ...) {
   .data$data <- dplyr::filter(.data$data, ...)
   .data
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr mutate
-mutate.tidy_daggity <- function(.data, ...) {
+mutate.tidy_dagitty <- function(.data, ...) {
   .data$data <- dplyr::mutate(.data$data, ...)
   .data
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr summarise
-summarise.tidy_daggity <- function(.data, ...) {
+summarise.tidy_dagitty <- function(.data, ...) {
   .data$data <- dplyr::summarise(.data$data, ...)
   .data
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr arrange
-arrange.tidy_daggity <- function(.data, ...) {
+arrange.tidy_dagitty <- function(.data, ...) {
   .data$data <- dplyr::arrange(.data$data, ...)
   .data
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr group_by
-group_by.tidy_daggity <- function(.data, ...) {
+group_by.tidy_dagitty <- function(.data, ...) {
   .data$data <- dplyr::group_by(.data$data, ...)
   .data
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr ungroup
-ungroup.tidy_daggity <- function(.data, ...) {
+ungroup.tidy_dagitty <- function(.data, ...) {
   .data$data <- dplyr::ungroup(.data$data, ...)
   .data
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr transmute
-transmute.tidy_daggity <- function(.data, ...) {
+transmute.tidy_dagitty <- function(.data, ...) {
   .data$data <- dplyr::transmute(.data$data, ...)
   .data
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr distinct
-distinct.tidy_daggity <- function(.data, ..., .keep_all = FALSE) {
+distinct.tidy_dagitty <- function(.data, ..., .keep_all = FALSE) {
   .data$data <- dplyr::distinct(.data$data, ..., .keep_all = .keep_all)
   .data
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr full_join
-full_join.tidy_daggity <- function(x, y, by = NULL, copy = FALSE, suffix = c(".x", ".y"), ...) {
+full_join.tidy_dagitty <- function(x, y, by = NULL, copy = FALSE, suffix = c(".x", ".y"), ...) {
   x$data <- dplyr::full_join(x$data, by = by, copy = copy, suffix = suffix, ...)
   x
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr inner_join
-inner_join.tidy_daggity <- function(x, y, by = NULL, copy = FALSE, suffix = c(".x", ".y"), ...) {
+inner_join.tidy_dagitty <- function(x, y, by = NULL, copy = FALSE, suffix = c(".x", ".y"), ...) {
   x$data <- dplyr::inner_join(x$data, by = by, copy = copy, suffix = suffix, ...)
   x
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr left_join
-left_join.tidy_daggity <- function(x, y, by = NULL, copy = FALSE, suffix = c(".x", ".y"), ...) {
+left_join.tidy_dagitty <- function(x, y, by = NULL, copy = FALSE, suffix = c(".x", ".y"), ...) {
   x$data <- dplyr::left_join(x$data, by = by, copy = copy, suffix = suffix, ...)
   x
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr right_join
-right_join.tidy_daggity <- function(x, y, by = NULL, copy = FALSE, suffix = c(".x", ".y"), ...) {
+right_join.tidy_dagitty <- function(x, y, by = NULL, copy = FALSE, suffix = c(".x", ".y"), ...) {
   x$data <- dplyr::right_join(x$data, by = by, copy = copy, suffix = suffix, ...)
   x
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr anti_join
-anti_join.tidy_daggity <- function(x, y, by = NULL, copy = FALSE, ...) {
+anti_join.tidy_dagitty <- function(x, y, by = NULL, copy = FALSE, ...) {
   x$data <- dplyr::anti_join(x$data, by = by, copy = copy, ...)
   x
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr semi_join
-semi_join.tidy_daggity <- function(x, y, by = NULL, copy = FALSE, ...) {
+semi_join.tidy_dagitty <- function(x, y, by = NULL, copy = FALSE, ...) {
   x$data <- dplyr::semi_join(x$data, by = by, copy = copy, ...)
   x
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr slice
-slice.tidy_daggity <- function(.data, ..., .dots = list()) {
+slice.tidy_dagitty <- function(.data, ..., .dots = list()) {
   .data$data <- dplyr::slice(.data$data, ...)
   .data
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr select_
-select_.tidy_daggity <- function(.data, ..., .dots = list()) {
+select_.tidy_dagitty <- function(.data, ..., .dots = list()) {
   .data$data <- dplyr::select_(.data$data, ...)
   .data
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr filter_
-filter_.tidy_daggity <- function(.data, ..., .dots = list()) {
+filter_.tidy_dagitty <- function(.data, ..., .dots = list()) {
   .data$data <- dplyr::filter_(.data$data, ...)
   .data
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr mutate_
-mutate_.tidy_daggity <- function(.data, ..., .dots = list()) {
+mutate_.tidy_dagitty <- function(.data, ..., .dots = list()) {
   .data$data <- dplyr::mutate_(.data$data, ...)
   .data
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr summarise_
-summarise_.tidy_daggity <- function(.data, ..., .dots = list()) {
+summarise_.tidy_dagitty <- function(.data, ..., .dots = list()) {
   .data$data <- dplyr::summarise_(.data$data, ...)
   .data
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr arrange_
-arrange_.tidy_daggity <- function(.data, ..., .dots = list()) {
+arrange_.tidy_dagitty <- function(.data, ..., .dots = list()) {
   .data$data <- dplyr::arrange_(.data$data, ...)
   .data
 }
 
+#' @name dplyr
 #' @export
 #' @importFrom dplyr slice_
-slice_.tidy_daggity <- function(.data, ..., .dots = list()) {
+slice_.tidy_dagitty <- function(.data, ..., .dots = list()) {
   .data$data <- dplyr::slice_(.data$data, ...)
   .data
 }
