fe4f1ab2cf0d294c3c3a328b542df3040c5ba786
Malcolm Barrett <malcolmbarrett@gmail.com>
2021-01-07 12:22:34 -0800

    replace helper dag name to avoid conflict with dag()

--- tests/testthat/test-colliders.R
+++ tests/testthat/test-colliders.R
@@ -2,19 +2,19 @@ context("ggdag_collider")
 set.seed(1234)
 
 test_that("colliders correctly identified", {
-  dag <- dagify(m ~ x + y, y ~ x)
-  p <- ggdag_collider(dag)
+  test_dag <- dagify(m ~ x + y, y ~ x)
+  p <- ggdag_collider(test_dag)
   expect_doppelganger("ggdag_collider() highlights `m`", p)
 })
 
 test_that("colliders and downstream colliders are detected", {
-  dag <- dagify(m ~ x + y, m_jr ~ m)
-  expect_true(is_collider(dag, "m"))
-  expect_true(is_downstream_collider(dag, "m_jr"))
-  expect_false(is_collider(dag, "x"))
-  expect_false(is_collider(dag, "y"))
-  expect_false(is_downstream_collider(dag, "x"))
-  expect_false(is_downstream_collider(dag, "y"))
+  test_dag <- dagify(m ~ x + y, m_jr ~ m)
+  expect_true(is_collider(test_dag, "m"))
+  expect_true(is_downstream_collider(test_dag, "m_jr"))
+  expect_false(is_collider(test_dag, "x"))
+  expect_false(is_collider(test_dag, "y"))
+  expect_false(is_downstream_collider(test_dag, "x"))
+  expect_false(is_downstream_collider(test_dag, "y"))
 })
 
 test_that("many colliders activated are processed correctly", {
--- tests/testthat/test-dagify.R
+++ tests/testthat/test-dagify.R
@@ -1,5 +1,5 @@
 test_that("dagify creates correct dagitty", {
-  dag <- dagify(y ~ x + z, x~ z)
-  expect_equal(dag[[1]], "dag {\nx\ny\nz\nx -> y\nz -> x\nz -> y\n}\n")
-  expect_s3_class(dag, "dagitty")
+  test_dag <- dagify(y ~ x + z, x~ z)
+  expect_equal(test_dag[[1]], "dag {\nx\ny\nz\nx -> y\nz -> x\nz -> y\n}\n")
+  expect_s3_class(test_dag, "dagitty")
 })
