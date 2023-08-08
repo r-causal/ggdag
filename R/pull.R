7fd663cec598b9fc93fa415c3691eb3ef1968f2a
Malcolm Barrett <malcolmbarrett@gmail.com>
2023-08-08 12:04:10 -0400

    add `pull_dag()` and `pull_dag_data()`

---
+++ R/pull.R
@@ -0,0 +1,53 @@
+#' Pull components from DAG objects
+#'
+#' `pull_dag()` and `pull_dag_data()` are generic methods to pull components of
+#' DAG objects, e.g. `tidy_dagitty`, such as the `dagitty` object or the data
+#' frame associated with it. These methods are recommended over extracting
+#' components manually, e.g. `my_dag$data`, because the internal structure of
+#' these objects may change over time.
+#'
+#' @param x a `tidy_dagitty` or `dagitty` object.
+#'
+#' @return a DAG object, e.g. `dagitty`, or data frame
+#'
+#' @examples
+#'
+#' tidy_dagitty_obj <- dagify(y ~ x + z, x ~ z)
+#' dag <- pull_dag(tidy_dagitty_obj)
+#' dag_data <- pull_dag_data(tidy_dagitty_obj)
+#'
+#' @export
+pull_dag <- function(x, ...) {
+  UseMethod("pull_dag")
+}
+
+#' @export
+#' @rdname pull_dag
+pull_dag.tidy_dagitty <- function(x, ...) {
+  x$dag
+}
+
+#' @export
+#' @rdname pull_dag
+pull_dag.dagitty <- function(x, ...) {
+  x
+}
+
+#' @export
+#' @rdname pull_dag
+pull_dag_data <- function(x, ...) {
+  UseMethod("pull_dag_data")
+}
+
+#' @export
+#' @rdname pull_dag
+pull_dag_data.tidy_dagitty <- function(x, ...) {
+  x$data
+}
+
+#' @export
+#' @rdname pull_dag
+pull_dag_data.dagitty <- function(x, ...) {
+  tidy_dagitty(x, ...)$data
+}
+
---
+++ tests/testthat/test-pull.R
@@ -0,0 +1,19 @@
+test_that("pull_dag and pull_dag_data return the correct objects", {
+  # Create a dagitty object
+  dag <- dagify(y ~ x + z, x ~ z)
+
+  # Create a tidy_dagitty object
+  tidy_dag <- tidy_dagitty(dag, seed = 1234)
+
+  # Test that pull_dag returns the correct dagitty object
+  expect_equal(pull_dag(dag), dag)
+
+  # Test that pull_dag returns the correct dagitty object from a tidy_dagitty object
+  expect_equal(pull_dag(tidy_dag), dag)
+
+  # Test that pull_dag_data returns the correct data frame
+  expect_equal(pull_dag_data(dag, seed = 1234), tidy_dag$data)
+
+  # Test that pull_dag_data returns the correct data frame from a tidy_dagitty object
+  expect_equal(pull_dag_data(tidy_dag), tidy_dag$data)
+})
