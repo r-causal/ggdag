735132b8284faddab54d961b659bac4bee19a696
Teun van den Brand <tahvdbrand@gmail.com>
2024-01-08 11:42:13 +0100

    add sentinel test

--- tests/testthat/test-StatsandGeoms.R
+++ tests/testthat/test-StatsandGeoms.R
@@ -13,5 +13,13 @@ test_that("Geom and Stat ggprotos are in fact ggprotos", {
   expect_ggproto(StatEdgeDiagonal)
   expect_ggproto(StatEdgeFan)
   expect_ggproto(GeomDAGEdgePath)
-  expect_ggproto(ScalesListQuiet)
+})
+
+test_that("We do not need to update `silent_add()`.", {
+  # This is a sentinel test to see if upstream ggplot2 has made changes to
+  # the ggplot2:::Scales$add() method.
+  # If this test fails, the add method has likely changed and `silent_add()`
+  # may need to be updated in StatsandGeoms.R.
+  body <- body(environment(ggplot()$scales$add)$f)
+  expect_snapshot(body)
 })
