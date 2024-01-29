56c7e80a5fa0d891098a766e85a8f50a01b583f4
Malcolm Barrett <malcolmbarrett@gmail.com>
2024-01-29 12:41:20 -0500

    conver equiv class

--- R/equivalence.R
+++ R/equivalence.R
@@ -155,21 +155,21 @@ node_equivalent_class <- function(.dag, layout = "auto") {
 #' @rdname equivalent
 #' @inheritParams expand_plot
 #' @export
-ggdag_equivalent_class <- function(.tdy_dag,
-                                   expand_x = expansion(c(.10, .10)),
-                                   expand_y = expansion(c(.10, .10)),
-                                   breaks = ggplot2::waiver(), ...,
+ggdag_equivalent_class <- function(.tdy_dag, ..., size = 1,
                                    node_size = 16, text_size = 3.88, label_size = text_size,
-                                   text_col = "white", label_col = text_col,
-                                   node = TRUE, stylized = FALSE,
-                                   text = TRUE, use_labels = NULL) {
+                                   text_col = "white", label_col = "black",
+                                   edge_width = 0.6, edge_cap = 8, arrow_length = 5,
+                                   use_edges = TRUE,
+                                   use_nodes = TRUE, use_stylized = FALSE, use_text = TRUE,
+                                   use_labels = FALSE, text = NULL, label = NULL,
+                                   node = deprecated(), stylized = deprecated()) {
   .tdy_dag <- if_not_tidy_daggity(.tdy_dag) %>%
     node_equivalent_class(...)
 
   reversable_lines <- dplyr::filter(pull_dag_data(.tdy_dag), reversable)
   non_reversable_lines <- dplyr::filter(pull_dag_data(.tdy_dag), !reversable)
   p <- .tdy_dag %>%
-    ggplot2::ggplot(ggplot2::aes(x = x, y = y, xend = xend, yend = yend, edge_alpha = reversable)) +
+    ggplot2::ggplot(aes_dag(edge_alpha = reversable)) +
     geom_dag_edges(
       data_directed = dplyr::filter(non_reversable_lines, direction != "<->"),
       data_bidirected = dplyr::filter(non_reversable_lines, direction == "<->")
@@ -178,24 +178,26 @@ ggdag_equivalent_class <- function(.tdy_dag,
     breaks(breaks) +
     ggraph::scale_edge_alpha_manual(name = "Reversable", drop = FALSE, values = c(.30, 1))
 
-  if (node) {
-    if (stylized) {
-      p <- p + geom_dag_node(size = node_size)
-    } else {
-      p <- p + geom_dag_point(size = node_size)
-    }
-  }
+  p <- p + geom_dag(
+    size = size,
+    node_size = node_size,
+    text_size = text_size,
+    label_size = label_size,
+    text_col = text_col,
+    label_col = label_col,
+    edge_width = edge_width,
+    edge_cap = edge_cap,
+    arrow_length = arrow_length,
+    use_edges = FALSE,
+    use_nodes = use_nodes,
+    use_stylized = use_stylized,
+    use_text = use_text,
+    use_labels = use_labels,
+    text = !!rlang::enquo(text),
+    label = !!rlang::enquo(label),
+    node = node,
+    stylized = stylized
+  )
 
-  if (text) p <- p + geom_dag_text(col = text_col, size = text_size)
-
-  if (!is.null(use_labels)) {
-    p <- p +
-      geom_dag_label_repel(
-        ggplot2::aes(label = !!rlang::sym(use_labels)),
-        size = text_size,
-        col = label_col,
-        show.legend = FALSE
-      )
-  }
   p
 }
