dd14ac9f489857da4d0d7575b6257e29bd69a562
Malcolm Barrett <malcolmbarrett@gmail.com>
2024-03-03 13:38:49 -0500

    manually strip graph attribute and test for nullness

--- R/tidy_dag.R
+++ R/tidy_dag.R
@@ -278,22 +278,23 @@ generate_layout <- function(.df, layout, vertices = NULL, coords = NULL, ...) {
 
 
   if (no_existing_coords) {
-    ggraph_layout <- suppressMessages(ggraph::create_layout(
+    ggraph_layout <- ggraph_create_layout(
       ig,
       layout = layout,
       ...
-    ))
+    )
+
   } else {
     nodes <- names(igraph::V(ig))
     coords$x <- coords$x[nodes]
     coords$y <- coords$y[nodes]
-    ggraph_layout <- suppressMessages(ggraph::create_layout(
+    ggraph_layout <- ggraph_create_layout(
       ig,
       layout = "manual",
       x = coords$x,
       y = coords$y,
       ...
-    ))
+    )
   }
 
   ggraph_layout %>%
